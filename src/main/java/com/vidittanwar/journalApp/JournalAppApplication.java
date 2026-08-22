ae5922df13ef59201152fa96f5a5a4dbfef20afc
Vidit Tanwar <vidittanwar4@gmail.com>
2026-08-22 23:17:40 +0530

    fix

--- src/main/java/com/vidittanwar/journalApp/JournalAppApplication.java
+++ src/main/java/com/vidittanwar/journalApp/JournalAppApplication.java
@@ -1,9 +1,9 @@
-
-
 package com.vidittanwar.journalApp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
@@ -23,5 +23,15 @@ public class JournalAppApplication {
 	@Bean
 	public PlatformTransactionManager falana(MongoDatabaseFactory dbFactory){
 		return new MongoTransactionManager(dbFactory);
+	}
+
+	@Bean
+	public CommandLineRunner checkMongoUri(@Value("${spring.data.mongodb.uri:NOT_FOUND}") String uri) {
+		return args -> {
+			System.out.println("=== MONGO URI CHECK ===");
+			System.out.println("Length: " + uri.length());
+			System.out.println("Starts with: " + uri.substring(0, Math.min(20, uri.length())));
+			System.out.println("=======================");
+		};
 	}
 }
